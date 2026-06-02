fb1eeac30b78ecbb13880921b75e579eabc655b4
Anucha Takham <anucha.tkhm@gmail.com>
2026-06-02 08:30:12 +0700

    fix: clippy needless_borrows_for_generic_args in limits_test

--- src/nostr/limits_test.rs
+++ src/nostr/limits_test.rs
@@ -17,7 +17,7 @@ use super::*;
     #[test]
     fn test_limits_serde_roundtrip() {
         let limits = Limits::new(99999, 42);
-        let json = serde_json::to_value(&limits).unwrap();
+        let json = serde_json::to_value(limits).unwrap();
         let deserialized: Limits = serde_json::from_value(json).unwrap();
         assert_eq!(deserialized.max_content_length, 99999);
         assert_eq!(deserialized.max_subscriptions_per_connection, 42);
